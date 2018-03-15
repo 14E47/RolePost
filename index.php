69a12ff8d23534b68fdb118469bd6dc6ea6167a1
14E47 <wasique3@gmail.com>
2018-03-15 13:14:56 +0530

    archive code

---
+++ index.php
@@ -0,0 +1,144 @@
+<html>
+<head>
+<title>Role Post :: News</title>
+<link rel="stylesheet" href="themes/rolepost/style.css" type="text/css">
+<link rel="shortcut icon" href="themes/rolepost/favicon.ico">
+<meta name="description" content="A free role playing forum and community.">
+<meta name="author" content="Kelsey Smith">
+<meta name="keywords" content="arzynik, gemini, the gemini portal, devin smith, php, free, opensource, website, my, gallery, forums, forum, comments, author, comment, post, thread, section, category, article, articles, download, downloads, link, links, weblink, weblinks, search, query, account, ArzFS, arzfs, database, mysql, poll, polls, user, users, about">
+<meta name="robots" content="index,follow">
+</head>
+
+<body><table border="0" cellpadding="0" cellspacing="0" width="100%" align="center" valign="top">
+<tr><td width=135><td valign=top align=center>
+<table border="0" cellpadding="0" cellspacing="0" width="770" align="center" valign="top"><tr><td valign=top>
+<tr height=182><td background="themes/rolepost/images/rp.jpg" valign=top height=182 bgcolor=#ffffff>
+<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" WIDTH="770" HEIGHT="182" id="menu" ALIGN="">
+<PARAM NAME=movie VALUE="themes/rolepost/rolepost.swf?user=Anonymous&sitename=Role Post&thisurl=/index.php">
+<PARAM NAME=quality VALUE=high>
+<param name="wmode" value="transparent">
+<EMBED src="themes/rolepost/rolepost.swf?user=Anonymous&sitename=Role Post&thisurl=/index.php" quality=high bgcolor=#FFFFFF  WIDTH="770" HEIGHT="182" NAME="bytesfullv2" ALIGN="" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED>
+</OBJECT>
+<tr><td width=100% valign=top>
+
+
+<table border="0" bgcolor=#ffffff cellpadding="3" cellspacing="3" width="100%" align="center" valign="top"><tr><td valign=top>
+<table class=menu1 width="100%" border="0" cellspacing="0" cellpadding="5"><tr><td>
+<table class=menu1 border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td><img src="images/pixel.gif" height="10" width="10">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=news" class="menu1">News</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1current"><a href="http://rolepost.com/index.php?name=forums" class="menu1">Forums</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=userinfo" class="menu1">Users</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=categories" class="menu1">Archive</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=links" class="menu1">Links</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=faq" class="menu1">FAQ</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=search" class="menu1">Search</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=submit" class="menu1">Submit</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=users" class="menu1">My Account</a>
+<td><img src="images/pixel.gif" height="10" width="20">
+<td class="menu1"><a href="http://rolepost.com/index.php?name=rss" class="menu1"><img src=images/xml.gif border=0></a>
+<td><img src="images/pixel.gif" height="10" width="20">
+</table>
+</table>
+
+<tr><td valign=top height=100%>
+
+
+<table width="100%" border="0" cellspacing="0" cellpadding="2">
+<tr class="menu2title"><td class="menu2title" width="100%" style="text-align: left;"><img src="images/pixel.gif" width=10 height=1><font class="columnwhitetitle">News</font><br>
+</table>
+<table border="0" cellpadding="1" cellspacing="0" width="100%" align="center" bgcolor="#999999" ><tr><td><table border="0" cellpadding="2" cellspacing="0" width="100%" align="center" bgcolor="#E3EBFF" >
+<tr><td>
+Displaying <b>1</b> through <b>2</b> of <b>2</b> articles.
+<table width=100% cellpadding=2 cellspacing=2 align=center>
+<tr><td valign=top ><table width=100%><tr class="menu2title" width=100%><td>
+<table cellpadding=0 cellspacing=0 class="menu2title" width=100%>
+<tr width=100%><td class="menu2title" nowrap><img src=pixel.gif width=3 height=1>
+<a href=index.php?name=categories&op=detail&cid=1><font class=menu2title>News</a> : New Hosting
+<td align=right class="menu2title2" width=100% no wrap>Thursday, March 18th 2004<img src=pixel.gif width=3 height=1></table>
+<tr><td><table cellpadding=0 cellspacing=0 width=100%><tr><td valign=top><font class=news>
+Posted by <a href=index.php?name=users&logop=info&uname=arzynik><font class=useradmin>arzynik</font></a><br><br>
+Rolepost.com will be rolocated soon.<br>The current host has expired and it will be relocated to a faster more reliable host.<br>Expect some downtime to accomadate a more relable system.<br>Sorry for the inconvience.<br><br>-Devin <td valign=top width=1><a href=index.php?name=categories&op=detail&cid=1>
+<img src="images/categories/news.jpg" alt="News" border=0></a></table>
+</font><tr><td><center><a href=index.php?name=articles&op=rate&aid=2>Rate</a> | 
+<a href=index.php?name=articles&op=comment&aid=2>Comment</a> | 
+<a href=index.php?name=articles&op=detail&aid=2>View</a> | 
+<a href=index.php?name=articles&op=print&aid=2>Print</a>
+<br>Rating: Not yet rated | Views: 3 | Comments: 0
+</center></table>
+
+
+<tr><td valign=top ><table width=100%><tr class="menu2title" width=100%><td>
+<table cellpadding=0 cellspacing=0 class="menu2title" width=100%>
+<tr width=100%><td class="menu2title" nowrap><img src=pixel.gif width=3 height=1>
+<a href=index.php?name=categories&op=detail&cid=1><font class=menu2title>News</a> : RolePost.com Live
+<td align=right class="menu2title2" width=100% no wrap>Friday, February 20th 2004<img src=pixel.gif width=3 height=1></table>
+<tr><td><table cellpadding=0 cellspacing=0 width=100%><tr><td valign=top><font class=news>
+Posted by <a href=index.php?name=users&logop=info&uname=arzynik><font class=useradmin>arzynik</font></a><br><br>
+Today RolePost.com goes live! <br><a href="index.php?name=users&logop=signup" target=_blank>Sign up here!</a><br><br>This is the new '<B>Gemini Portal v3.0</B>' so <I>there are bugs</I>!<br>Please submit all bugs or site problems <a href="http://arzynik.com/index.php?name=forums&mini=&op=section&sid=13" target=_blank>here</a>.<br><br>This site was created out of my own loving heart (wich isnt very loving) for free and i am poor.<br>If you would like to help and support my food money raising fund please click the google ad links.<br>I figured no one will ever donate so ill put thoes there.<br><br>Thanks so much, <br>and have an awsome time on <B>RolePost.com</B>! <td valign=top width=1><a href=index.php?name=categories&op=detail&cid=1>
+<img src="images/categories/news.jpg" alt="News" border=0></a></table>
+</font><tr><td><center><a href=index.php?name=articles&op=rate&aid=1>Rate</a> | 
+<a href=index.php?name=articles&op=comment&aid=1>Comment</a> | 
+<a href=index.php?name=articles&op=detail&aid=1>View</a> | 
+<a href=index.php?name=articles&op=print&aid=1>Print</a>
+<br>Rating: Not yet rated | Views: 22 | Comments: 0
+</center></table>
+
+
+</table>
+<table width=100%><tr><td align=left>
+<td align=right>Show 
+<a href=index.php?name=news&p=1&m=5>5</a>,
+10,
+<a href=index.php?name=news&p=1&m=25>25</a>,
+<a href=index.php?name=news&p=1&m=50>50</a>
+ results per page.
+</table>
+</table></table>
+
+</table><tr><td valign=top>
+<br><br>
+<table cellpadding=0 cellspacing=0 valign=top  border=0><tr><td width=100%><td nowrap valign=top><font class=copyright>
+Copyright &copy 2004 <a href=http://rolepost.com>RolePost.com</a><br>All logos and trademarks in this site are property of their respective owners.<br>
+The comments and posts are property of their posters.<br>
+Web site's engine is Copyright &copy; 2003-2004 <a href=http://arzynik.com>Arzynik</a>
+
+    </table></table><td width=15><img src=images/pixel.gif width=15><td valign=top align=right>
+<script type="text/javascript"><!--
+google_ad_client = "pub-7289853541435839";
+google_ad_width = 120;
+google_ad_height = 600;
+google_ad_format = "120x600_as";
+google_color_border = "333333";
+google_color_bg = "000000";
+google_color_link = "FFFFFF";
+google_color_url = "999999";
+google_color_text = "CCCCCC";
+//--></script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<br><br><table width=100%><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="paypal@arzynik.com">
+<input type="hidden" name="item_name" value="RolePost.com Donation">
+<input type="hidden" name="item_number" value="1">
+<input type="hidden" name="no_shipping" value="1">
+<input type="hidden" name="return" value="http://rolepost.com/index.php?name=donate&op=thank">
+<input type="hidden" name="cancel_return" value="http://rolepost.com/index.php?name=donate&op=cancel">
+<input type="hidden" name="cn" value="Your contributions are greatly appreciated.">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="tax" value="0">
+<input type="hidden" name="lc" value="US">
+<input type="image" src="images/x-click-butcc-donate.gif" border="0" name="submit" alt="PayPal Donation">
+</form></table>
+</table></body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>RolePost &rsaquo; Log In</title>
+	<link rel='stylesheet' id='buttons-css'  href='http://rolepost.com/wp-includes/css/buttons.min.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://rolepost.com/wp-includes/css/dashicons.min.css?ver=4.3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://rolepost.com/wp-admin/css/login.min.css?ver=4.3.1' type='text/css' media='all' />
+<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='wpsf_wpsf-css'  href='http://rolepost.com/wp-content/plugins/wp-spam-fighter/css/wpsf.css?ver=0.5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='wpclef-main-css'  href='http://rolepost.com/wp-content/plugins/wpclef/assets/dist/css/main.min.css?ver=2.3.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://rolepost.com/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
+<script type='text/javascript' src='http://rolepost.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='https://www.google.com/recaptcha/api.js?ver=4.3.1'></script>
+<meta name='robots' content='noindex,follow' />
+
+<script type='text/javascript' src='http://rolepost.com/wp-content/plugins/wp-spamshield/js/jscripts.php'></script> 
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+	<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">RolePost</a></h1>
+	
+<form name="loginform" id="loginform" action="http://rolepost.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://rolepost.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://rolepost.com/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://rolepost.com/" title="Are you lost?">&larr; Back to RolePost</a></p>
+	
+	</div>
+
+	
+	
+<script type='text/javascript'>
+/* <![CDATA[ */
+r3f5x9JS=escape(document['referrer']);
+hf4N='f427d07c342f0309a194e2b88d8048d4';
+hf4V='170e6542b8f0d17d610072541797da0d';
+jQuery(document).ready(function($){var e="#commentform, .comment-respond form, .comment-form, #lostpasswordform, #registerform, #loginform, #login_form, #wpss_contact_form";$(e).submit(function(){$("<input>").attr("type","hidden").attr("name","r3f5x9JS").attr("value",r3f5x9JS).appendTo(e);$("<input>").attr("type","hidden").attr("name",hf4N).attr("value",hf4V).appendTo(e);return true;});$("#comment").attr({minlength:"15",maxlength:"15360"})});
+/* ]]> */
+</script> 
+<script type='text/javascript' src='http://rolepost.com/wp-content/plugins/wp-spam-fighter/javascript/wp-spamfighter.js?ver=0.5.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var clefTranslations = {"messages":{"error":{"connect":"There was a problem automatically connecting your Clef account: <%= error %>. Please refresh and try again.","create":"There was a problem creating a new Clef application for your WordPress site: <%= error %>. Please refresh and try again. If the issue, persists, email <a href='mailto:support@getclef.com'>support@getclef.com<\/a>.","invite":"There was a problem sending invites: <%= error %>.","generic":"Something wrong, please refresh and try again.","disconnect":"There was a problem disconnecting your Clef account: <%= error %>."},"success":{"connect":"You've successfully connected your account with Clef","configured":"You're all set up!","invite":"Email invitations have been sent to your users.","disconnect":"Successfully disconnected Clef account."},"saving":"Settings are being saved. Are you sure you want to navigate away?"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://rolepost.com/wp-content/plugins/wpclef/assets/dist/js/login.min.js?ver=2.3.1'></script>
+<script type='text/javascript' src='http://rolepost.com/wp-content/plugins/wp-spamshield/js/jscripts-ftr-min.js'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
